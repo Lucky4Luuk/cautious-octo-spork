9e4e00e9c4eaf579633334e5f04ef765b9c5c01f
Luuk van Oijen <lazyluuk.channel@gmail.com>
2019-09-19 14:19:11 +0200

    get_block_id
    should work, havent tested it yet

--- rust-chunk/src/lib.rs
+++ rust-chunk/src/lib.rs
@@ -41,7 +41,7 @@ py_class!(class Chunk |py| {
         Ok(py_block_data)
     }
 
-    def set_block(&self, x: usize, y: usize, z: usize, block_id: u16) -> PyResult<bool> {
+    def set_block_id(&self, x: usize, y: usize, z: usize, block_id: u16) -> PyResult<bool> {
         let ref mut tmp_block_data = *(self.block_data(py).borrow_mut());
         tmp_block_data[x][y][z] = block_id;
         self.block_data(py).replace(tmp_block_data.to_vec());
@@ -59,7 +59,6 @@ py_class!(class Chunk |py| {
                     let data_result = section.get_item(py, 0);
                     if data_result.is_ok() {
                         let block_id = data_result.unwrap().extract::<u16>(py)?;
-                        // Chunk::set_block(self, x as usize, y as usize + (section_id as usize) * 16, z as usize, block_id);
                         let ref mut tmp_block_data = *(self.block_data(py).borrow_mut());
                         tmp_block_data[x as usize][z as usize + (section_id as usize) * 16][y as usize] = block_id;
                         self.block_data(py).replace(tmp_block_data.to_vec());
@@ -69,8 +68,11 @@ py_class!(class Chunk |py| {
         }
 
         Ok(true)
+    }
 
-        //Ok(section.get_type(py).name(py).into_owned())
+    def get_block_id(&self, x: usize, y: usize, z: usize) -> PyResult<u16> {
+        let block_id: u16 = self.block_data(py).borrow()[x][y][z];
+        Ok(block_id)
     }
 
 });
